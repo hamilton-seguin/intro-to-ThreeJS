d5cfec0561e46f55a15bfc0bc235b08c2c27b8ca
Hamilton <>
2023-10-15 18:34:37 +0200

    add texture offset and repeatwrapping

--- mesh-textures/src/script.js
+++ mesh-textures/src/script.js
@@ -8,13 +8,46 @@ const pane = new Pane();
 // initialize the scene
 const scene = new THREE.Scene();
 
+// initialize group
+const group = new THREE.Group();
+
+// initialize the texture loader
+const textureLoader = new THREE.TextureLoader();
+
 // initialize the geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+
+// initialize the texture
+const grassTexture = textureLoader.load(
+  "/textures/space-cruiser-panels2-bl/space-cruiser-panels2_albedo.png"
+);
+grassTexture.repeat.set(2, 2);
+
+grassTexture.wrapS = THREE.RepeatWrapping;
+grassTexture.wrapT = THREE.RepeatWrapping;
+
+
+pane.addBinding(grassTexture, "offset", {
+  x: {
+    min: -1,
+    max: 1,
+    step: 0.01,
+  },
+  y: {
+    min: -1,
+    max: 1,
+    step: 0.01,
+  },
+});
 
 // initialize the material
-const material = new THREE.MeshBasicMaterial();
+const material = new THREE.MeshPhysicalMaterial();
+material.side = THREE.DoubleSide;
+material.map = grassTexture;
 
 // initialize the mesh
 const cube = new THREE.Mesh(geometry, material);
@@ -23,29 +56,41 @@ const knot = new THREE.Mesh(torusKnotGeometry, material);
 knot.position.x = 1.5;
 
 const plane = new THREE.Mesh(planeGeometry, material);
-plane.position.x = -1.5;
+plane.rotation.x = -(Math.PI * 0.5);
+plane.scale.set(100, 100, 100);
+
+const sphere = new THREE.Mesh(); //same but add geometry and material after initialization
+sphere.geometry = sphereGeometry;
+sphere.material = material;
+sphere.position.y = 1.5;
+
+const cylinder = new THREE.Mesh();
+cylinder.geometry = cylinderGeometry;
+cylinder.material = material;
+cylinder.position.y = -1.5;
 
 // add the mesh to the scene
-scene.add(cube);
-scene.add(knot);
-scene.add(plane);
+// group.add(cube, knot, plane, sphere, cylinder);
+group.add(plane);
+scene.add(group);
 
 // initialize the light
-const light = new THREE.AmbientLight(0xffffff, 0.4);
+const light = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(light);
 
 const pointLight = new THREE.PointLight(0xffffff, 1.2);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(50, 50, 50);
 scene.add(pointLight);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
-  0.1,
-  200
+  1,
+  20000
 );
 camera.position.z = 10;
+camera.position.y = 5;
 
 // initialize the renderer
 const canvas = document.querySelector("canvas.threejs");
@@ -59,6 +104,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+// controls.autoRotate = true;
+/** this rotates the camera around the scene */
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -68,6 +115,16 @@ window.addEventListener("resize", () => {
 
 // render the scene
 const renderloop = () => {
+  // group.rotation.y += 0.01;
+  /** this rotates whole group on 1 y axis (fix camera)  */
+
+  // group.children.forEach((child) => {
+  //   if (child instanceof THREE.Mesh) {
+  //     child.rotation.y += 0.01;
+  //   }
+  // });
+  /**  this rotates each mesh in the group on its y axis */
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
