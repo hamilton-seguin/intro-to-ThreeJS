c57ebe9a66ed879c0ad02e5ec0ed460f58829ed7
Hamilton <>
2023-10-15 16:18:04 +0200

    mesh basic material

--- mesh-material/src/script.js
+++ mesh-material/src/script.js
@@ -10,22 +10,45 @@ const scene = new THREE.Scene();
 
 // initialize the geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(1, 1); 
+
+const torusGeometry = new THREE.TorusKnotGeometry(10, 3, 100, 16)
 
 // initialize the material
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
 });
 
+
+// material.color.set("violet"); // or new THREE.Color("violet")
+// material.transparent = true;
+// material.opacity = 1/3;
+material.side = THREE.DoubleSide; // ThreeJS constant, log to 2
+
+
 // initialize the mesh
 const mesh = new THREE.Mesh(geometry, material);
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -1.5;
+
+const torus = new THREE.Mesh(torusGeometry, material);
+torus.scale.set(0.1, 0.1, 0.1);
+torus.position.x = 3;
+
 scene.add(mesh);
+
+scene.add(plane);
+scene.add(torus);
+scene.fog = new THREE.Fog("white", 1, 10);
+scene.background = new THREE.Color("white");
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
-  0.1,
-  200
+  1,
+  20
 );
 camera.position.z = 5;
 
