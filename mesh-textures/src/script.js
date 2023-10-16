75f8a6a67fc316567c6735621af185c52ba91eeb
Hamilton <>
2023-10-16 14:34:17 +0200

    change to mappingtest to learn UV map

--- mesh-textures/src/script.js
+++ mesh-textures/src/script.js
@@ -22,9 +22,11 @@ const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
 const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
 
 // initialize the texture
+  /** // grasstexture on plane, repeat, offset
 const grassTexture = textureLoader.load(
   "/textures/space-cruiser-panels2-bl/space-cruiser-panels2_albedo.png"
 );
+
 grassTexture.repeat.set(2, 2);
 
 grassTexture.wrapS = THREE.RepeatWrapping;
@@ -43,11 +45,15 @@ pane.addBinding(grassTexture, "offset", {
     step: 0.01,
   },
 });
+*/
+
+const texture = textureLoader.load("/textures/uvMappingTest.jpg")
 
 // initialize the material
+
 const material = new THREE.MeshPhysicalMaterial();
 material.side = THREE.DoubleSide;
-material.map = grassTexture;
+material.map = texture;
 
 // initialize the mesh
 const cube = new THREE.Mesh(geometry, material);
@@ -56,8 +62,9 @@ const knot = new THREE.Mesh(torusKnotGeometry, material);
 knot.position.x = 1.5;
 
 const plane = new THREE.Mesh(planeGeometry, material);
-plane.rotation.x = -(Math.PI * 0.5);
-plane.scale.set(100, 100, 100);
+plane.position.x = -1.5;
+// plane.rotation.x = -(Math.PI * 0.5);
+// plane.scale.set(100, 100, 100);
 
 const sphere = new THREE.Mesh(); //same but add geometry and material after initialization
 sphere.geometry = sphereGeometry;
@@ -70,9 +77,10 @@ cylinder.material = material;
 cylinder.position.y = -1.5;
 
 // add the mesh to the scene
-// group.add(cube, knot, plane, sphere, cylinder);
-group.add(plane);
+group.add(cube, knot, plane, sphere, cylinder);
+// group.add(plane);
 scene.add(group);
+console.log(scene);
 
 // initialize the light
 const light = new THREE.AmbientLight(0xffffff, 0.6);
@@ -118,11 +126,11 @@ const renderloop = () => {
   // group.rotation.y += 0.01;
   /** this rotates whole group on 1 y axis (fix camera)  */
 
-  // group.children.forEach((child) => {
-  //   if (child instanceof THREE.Mesh) {
-  //     child.rotation.y += 0.01;
-  //   }
-  // });
+  group.children.forEach((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.rotation.y += 0.01;
+    }
+  });
   /**  this rotates each mesh in the group on its y axis */
 
   controls.update();
